cebc4e3bbcd54d8155584ddbfbdb6f19abe7d816
Alessandro T Busanello <alessandrodidi@hotmail.com>
2022-01-15 07:38:22 -0400

    Criação formulário principal

--- UPrincipal.pas
+++ UPrincipal.pas
@@ -4,10 +4,20 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.ComCtrls;
 
 type
   TfrmPrincipal = class(TForm)
+    mmMenuPrincipal: TMainMenu;
+    miSistema: TMenuItem;
+    miSisSair: TMenuItem;
+    miArquivo: TMenuItem;
+    miArqPessoa: TMenuItem;
+    miProcesso: TMenuItem;
+    miProcDoacao: TMenuItem;
+    miRelatorio: TMenuItem;
+    sbBarraStatusPrincipal: TStatusBar;
+    miRelDoacao: TMenuItem;
   private
     { Private declarations }
   public
